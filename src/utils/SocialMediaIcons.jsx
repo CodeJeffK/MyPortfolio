f040f687c6a43fe299588aa329819883871a6603
Sloth <jkasahara206@gmail.com>
2023-01-09 17:28:16 -0800

    skills, icons, gradient

---
+++ src/utils/SocialMediaIcons.jsx
@@ -0,0 +1,45 @@
+import linkedin from '../assets/linkedin.png';
+import github from '../assets/github.png';
+import facebook from '../assets/facebook.png';
+import instagram from '../assets/instagram.png';
+
+const SocialMediaIcons = () => {
+  return (
+    <div className="flex justify-center md:justify-start my-10 gap-7">
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://www.linkedin.com/in/jeff-kasahara-b7b120243/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img alt="linkedin-link" src={linkedin} />
+      </a>
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://github.com/CodeJeffK"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img alt="github-link" src={github} />
+      </a>
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://www.facebook.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img alt="facebook-link" src={facebook} />
+      </a>
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://www.instagram.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img alt="instagram-link" src={instagram} />
+      </a>
+    </div>
+  );
+};
+
+export default SocialMediaIcons;
